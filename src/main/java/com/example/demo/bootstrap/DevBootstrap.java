b8968eca23552e2a81738359e78214907d576781
DESKTOP-KGVGUPG\PAWEL <pablo.radziun@gmail.com>
2017-11-08 23:13:33 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/bootstrap/DevBootstrap.java
@@ -0,0 +1,59 @@
+package com.example.demo.bootstrap;
+
+import com.example.demo.model.Author;
+import com.example.demo.model.Book;
+import com.example.demo.model.Publisher;
+import com.example.demo.repositories.AuthorRepository;
+import com.example.demo.repositories.BookRepository;
+import com.example.demo.repositories.PublisherRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData() {
+        //Publisher
+        Publisher publisher = new Publisher();
+        publisher.setName("publisherName");
+        publisher.setAddress("publisherAddress");
+
+        publisherRepository.save(publisher);
+
+        //Eric
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "1234", publisher);
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        //Rod
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "234r", publisher);
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+
+    }
+
+
+}
